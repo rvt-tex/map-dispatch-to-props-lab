eccd4164eb0ad80e380f15a3f2743437d3f4cd0e
github username <rvtingling@yahoo.com>
2021-03-24 18:00:06 -0400

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -49,6 +49,14 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addRestaurant: restaurant => {
+      dispatch(addRestaurant(restaurant));
+    }
+  };
+}
+
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(state => ({ state }),mapDispatchToProps)(RestaurantInput);
